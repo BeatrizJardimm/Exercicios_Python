2a656a3210a9d6a42f24c21798ef82b30df03c71
Beatriz Jardim <paulabeatrizlj@hotmail.com>
2022-06-30 11:17:44 -0300

    Desafio finalizado

--- python/Dia7_desafio.py
+++ python/Dia7_desafio.py
@@ -1,54 +1,26 @@
 '''
------------------------------------------------------------------------------------------
-
 Desafio final de leitura de dados em CSV e JSON:
 
-- Em Python crie uma aplicação que mostre elementos específicos em uma tabela periódica:
-
-A aplicação deve ter no mínimo 15 elementos da tabela periódica, não precisa ser todos pois são muitos, escolha os que você quiser.
-
-Elementos Actinídeos
-
-89 - actínio (Ac)
-90 - tório (Th)
-91 - protactínio (Pa)
-92 - urânio (U)
-93 - netúnio (Np)
-94 - plutônio (Pu)
-95 - amerício (Am)
-96 - cúrio (Cm)
-97 - berquélio (Bk)
-98 - califórnio (Cf)
-99 - einstéinio (Es)
-100 - férmio (Fm)
-101 - mendelévio (Md)
-102 - nobélio (No)
-103 - laurêncio (Lr)
-
-Crie um CSV com um header contendo:
-
-Simbolo, Nome, NumeroAtomico, Linha, Coluna, EstadoFisico.
-
-E adicione abaixo nas colunas os respectivos dados retirados do seguinte site: https://ptable.com/#Propriedades
-
+1- Em Python crie uma aplicação que mostre elementos específicos em uma tabela periódica:
+2- A aplicação deve ter no mínimo 15 elementos da tabela periódica, não precisa ser todos pois são muitos, escolha os que você quiser.
+3- Crie um CSV com um header contendo: Simbolo, Nome, NumeroAtomico, Linha, Coluna, EstadoFisico.
+4- E adicione abaixo nas colunas os respectivos dados retirados do seguinte site: https://ptable.com/#Propriedades
 Exemplo:
-
 Simbolo, Nome, NumeroAtomico, Linha, Coluna, EstadoFisico.
-
 Cr, Cromio, 24, 4, 6, Solido
 
-Faça isso com no mínimo 15 elementos, aí já terá o CSV pronto para manipular.
+--> Para montar o arquivo, escolhi todos os elementos radioativos conhecidos
+--> Uma lista desses elementos pode ser encontrada em: https://brasilescola.uol.com.br/quimica/elementos-radioativos.htm
 '''
 
 #importando a biblioteca Pandas
 import pandas as pd
 
 #usa pandas para ler o arquivo csv, especificando qual é o separador dos objetos
-elementos = pd.read_csv("./csv/elementosActinideos.csv", encoding="utf-8", sep=',')
+elementos = pd.read_csv("./csv/elementosRadioativos.csv", encoding="utf-8", sep=',')
 
 #A) Crie uma “aplicação” em loop que tenha uma opção para listar todos os elementos da tabela, porém mostrando apenas uma propriedade do elemento. Ex: listar todos os nomes dos elementos na tabela.
 
-'''
 def questaoA():
 
     #variavel para entrar/sair do loop
@@ -86,41 +58,6 @@ def questaoA():
             case 7: #sair do loop
                 print("Até mais :)")    
                 loop = False #seta a variável loop para False e entao sai do loop
-'''
-
-def questaoA():
-
-    loop = True
-
-    while loop:
-        print("Bem-vindo à minha lista de elementos radioativos!!!")
-        print("Escolha uma das propriedades abaixo para mostrar seu valor em todos os elementos da tabela:")
-        print("1 - Símbolo")
-        print("2 - Nome")
-        print("3 - Número Atômico")
-        print("4 - Linha")
-        print("5 - Coluna")
-        print("6 - Estado Físico")
-        print("7 - Sair da aplicação")
-        propriedade = int(input("Entrada: "))
-
-        match propriedade:
-            case 1:
-                print(elementos.loc[:, "Simbolo"])
-            case 2:
-                print(elementos.loc[:, "Nome"])
-            case 3:
-                print(elementos.loc[:, "NumeroAtomico"])
-            case 4:
-                print(elementos.loc[:, "Linha"])
-            case 5:
-                print(elementos.loc[:, "Coluna"])
-            case 6:
-                print(elementos.loc[:, "EstadoFisico"])
-            case 7:
-                print("Até mais :)")
-                loop = False
-
 
 
 #B) Listar todos os dados de determinado elemento, buscando através do seu símbolo.
@@ -139,6 +76,7 @@ def questaoB():
     else:
         print("Este não é um símbolo válido")
 
+
 #C) Listar todos os dados de todos os elementos inseridos.
 
 def questaoC():
@@ -151,7 +89,7 @@ def questaoC():
 ########## ÁREA DE EXECUÇÃO DOS EXERCÍCIOS ##########
 
 print("Questão A: ")
-# questaoA()
+questaoA()
 print('\n')
 
 print("Questão B: ")
@@ -159,5 +97,5 @@ questaoB()
 print('\n')
 
 print("Questão C: ")
-#questaoC()
+questaoC()
 print('\n')
